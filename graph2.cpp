b5b33eae8374a945cbad15268606188cf378b5b8
mad1orz <99039151+mad1orz@users.noreply.github.com>
2022-02-06 14:49:53 +0530

    commit

--- graph2.cpp
+++ graph2.cpp
@@ -246,3 +246,49 @@ signed main()
     }
     return 0;
 }
+
+
+//dijkstra algo
+
+const int N = 1e5+10;
+const int INF = 1e9+7;
+
+vector<pair<int,int>> g[N];
+vector<int> vis(N, 0);
+vector<int> dis(N, INF);
+
+void dijkstra() {
+	set<pair<int,int>> st;
+	st.insert({0, source});
+	dis[source] = 0;
+
+	while (s.size() > 0) {
+		auto node = *st.begin();
+		int v = node.second;
+		int v_dis = node.first;
+		st.erase(st.begin());
+		if (vis[v]) continue;
+		vis[v] = 1;
+		for (auto child : g[v]) {
+			int child_v = child.first;
+			int wt = child.second; 
+			if (dis[v] + wt < dis[child_v]) {
+				dis[child_v] = dis[v] + wt;
+				st.insert({dis[child_v], child_v});
+			}
+		}
+	}
+
+}
+
+void solve() {
+	int n, m;
+	cin >> n >> m;
+	for (int i = 0; i < m; i++) {
+		int x, y, wt;
+		cin >> x >> y >> wt;
+		g[x].push_back({y, wt});
+		g[y].push_back({x, wt});
+	}
+}
+
