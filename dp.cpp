bfddd2a7a456e3ac8b74220c0ec5f64700873428
einsnicht <99039151+einsnicht@users.noreply.github.com>
2022-04-03 18:04:09 +0530

    commit

--- dp.cpp
+++ dp.cpp
@@ -27,18 +27,18 @@ void solve() {
 int dp[100010];
 
 int fib(int n) {
-  if (n == 0) return 0;
-  if (n == 1) return 1;
-  if (dp[n] != -1) return dp[n];
-  //memoise
-  return dp[n] = fib(n-1) + fib(n-2);
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+    if (dp[n] != -1) return dp[n];
+    //memoise
+    return dp[n] = fib(n-1) + fib(n-2);
 }
 
 void solve() {
-        memset(dp, -1, sizeof(dp));
-        int n;
-        cin >> n;
-        cout << fib(n);
+    memset(dp, -1, sizeof(dp));
+    int n;
+    cin >> n;
+    cout << fib(n);
 }
 
 
