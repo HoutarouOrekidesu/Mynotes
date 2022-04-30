096f66ddc1d709deb00742e3bb07eda47fcec69c
xororz <99039151+xororz@users.noreply.github.com>
2022-05-01 01:39:01 +0530

    commit

---
+++ mistakes.cpp
@@ -0,0 +1,9 @@
+while loop for taking out amout of times a certain number can be deducted from a given number is same as division.
+Along wiht while loop can be get tle.
+e.g https://codeforces.com/problemset/problem/1646/A
+
+mistake in the edge case handling of the idea I am using 
+https://codeforces.com/contest/1644/problem/B
+
+errors of handling of the indices of the array [1 base and 0 base array]
+https://codeforces.com/problemset/problem/1646/B
