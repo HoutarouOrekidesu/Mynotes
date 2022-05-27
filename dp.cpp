bfd71dcc3d8c0f6cf5f80fe11f5fb7b1f7ffcf39
xororz <99039151+xororz@users.noreply.github.com>
2022-05-27 22:09:03 +0530

    commit

--- dp.cpp
+++ dp.cpp
@@ -313,3 +313,34 @@ int main() {
   cout << dp[n][m] << "\n";
   return 0;
 }
+
+//longest increasing subsequence 
+
+#include <bits/stdc++.h>
+using namespace std;
+int n, dp[100];
+vector<int> a(100);
+int lis(int i) {
+    if (dp[i] != -1) return dp[i];
+    int ans = 1;
+    for (int j = 0; j < i; j++) {
+        if (a[i] > a[j]) ans = max(ans, lis(j) + 1);
+    }
+    return dp[i] = ans;
+}
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+  
+  memset(dp, -1, sizeof(dp));
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    cin >> a[i];
+  }
+  int flg = 0;
+  for (int i = 0; i < n; i++) {
+    flg = max(flg, lis(i));
+  }
+  cout << flg;
+  return 0;
+}
