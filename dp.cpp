ca3554e91b63f78c31de0ae947cda1c7383a12e9
xororz <99039151+xororz@users.noreply.github.com>
2022-05-27 22:05:29 +0530

    commit

--- dp.cpp
+++ dp.cpp
@@ -277,4 +277,39 @@ int main() {
   cout << "\n";
   return 0;
 }
+====================================================================================================================================================
 
+EDIT DISTANCE
+
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(nullptr);
+ 
+  string a, b;
+  cin >> a >> b;
+  a = '1' + a;
+  b = '1' + b;
+  int n = a.size() - 1, m = b.size() - 1;
+  vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+  for (int i = 0; i <= n; i++) {
+    for (int j = 0; j <= m; j++) {
+      if (i == 0) {
+        dp[i][j] = j;
+      }
+      else if (j == 0) {
+        dp[i][j] = i;
+      }
+      else if (a[i] == b[j]) {
+        dp[i][j] = dp[i - 1][j - 1];
+      }
+      else {
+        dp[i][j] = min({dp[i - 1][j], dp[i][j - 1], dp[i - 1][j - 1]}) + 1;
+      }
+    }
+  }
+  cout << dp[n][m] << "\n";
+  return 0;
+}
