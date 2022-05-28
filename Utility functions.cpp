f3323b45636d7c4c06c9ffd0de86dc1dc70cdadb
xororz <99039151+xororz@users.noreply.github.com>
2022-05-29 00:04:33 +0530

    commit

---
+++ Utility functions.cpp
@@ -0,0 +1,19 @@
+//Whole package to deal with long nCrs
+  fac[0] = 1;
+  for (int i = 1; i < 1000001; i++) {
+    fac[i] = (fac[i - 1] * i) % mod;
+  }
+  auto powmod = [&] (int a, int b) {
+    int res=1;
+    a %= mod;
+    for(;b;b>>=1){
+      if (b&1) {
+        res = (res * a) % mod;
+      }
+      a = (a*a) % mod;
+    }
+    return res;
+  };
+  auto binom = [&] (int x, int y) {
+    return fac[x] * powmod(fac[y] * fac[x-y], mod - 2) % mod;
+  };
