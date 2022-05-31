860c127da307d469c8aa3c9ec07f5632847662c4
xororz <99039151+xororz@users.noreply.github.com>
2022-06-01 02:11:55 +0530

    commit

--- policy base.cpp
+++ policy base.cpp
@@ -2,6 +2,12 @@
 using namespace __gnu_pbds;
 typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
 
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef tree<int,null_type,less<int>,rb_tree_tag, tree_order_statistics_node_update> indexed_set;
+
 indexed_set s;
 s.insert(2);  
 s.insert(3);
