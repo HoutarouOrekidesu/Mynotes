59a94e0029ac0cea1495bb5529a709a13c60cce7
Gaurav Chauhan <99039151+einsnicht@users.noreply.github.com>
2022-09-30 11:01:25 +0530

    commit

--- utility.cpp
+++ utility.cpp
@@ -5,3 +5,25 @@ string del(string s, int i) {
     n = s.substr(i+1, s.size()-i-1);
     return m+n;
 }
+
+int mod = 998244353;
+int fac[1000010];
+fac[0] = 1;
+for (int i = 1; i < 1000001; i++) {
+  fac[i] = (fac[i - 1] * i) % mod;
+}
+auto powmod = [&] (long long a, long long b) {
+  long long res = 1;
+  a %= mod;
+  for(; b; b >>= 1) {
+    if (b & 1) {
+      res = (res * a) % mod;
+    }
+    a = (a * a) % mod;
+  }
+  return res;
+};
+
+
+
+
