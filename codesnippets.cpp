bbf7e0a911d08c220cec7d65b2a9ad8c7e1968c2
Gaurav Chauhan <99039151+einsnicht@users.noreply.github.com>
2023-03-01 22:27:09 +0530

    commit

--- codesnippets.cpp
+++ codesnippets.cpp
@@ -103,3 +103,11 @@ for(int i = 0; i <= 60; i++) ncr[i][0] = 1;
   for(int i = 1; i <= 60; i++) {
     for(int j = 1; j <= i; j++) ncr[i][j] = (ncr[i - 1][j] + ncr[i - 1][j - 1]) % MOD;
 }
+  
+string del(string s, int i) {
+    if (i == 0) return s.substr(1, s.size()-1);
+    if (i == s.size()-1) return s.substr(0, s.size()-1);
+    string m = s.substr(0, i),
+    n = s.substr(i+1, s.size()-i-1);
+    return m+n;
+}
