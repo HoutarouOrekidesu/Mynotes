5eeaa71451c1da5cd17d29085180cd0a1229a6d0
Gaurav Chauhan <99039151+einsnicht@users.noreply.github.com>
2023-03-04 22:10:57 +0530

    commit

--- number theory.cpp
+++ number theory.cpp
@@ -112,10 +112,10 @@ vector<int> divisors[N];
 //sieve algorithm
 void sieve(int n) {
 	is_prime[0] = is_prime[1] = false;
-	for (int i = 2; i <= n; ++i) {
+	for (int i = 2; i <= n; i++) {
 		if (is_prime[i] == true) {
 			lp[i] = hp[i] = i;
-			for (int j = 2*i; j <= n; j += i){
+			for (int j = 2 * i; j <= n; j += i){
 				is_prime[j] = false; hp[j] = i;
 				if (lp[j] == 0) lp[j] = i;
 			}
